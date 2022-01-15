752ab2eeb0a8642ae7d54364408606d5a09b6138
Kim Weible <kw00183@my.westga.edu>
2022-01-15 10:16:11 -0600

    add variables for the product and divide problems, add code to the StartTheQuiz method that randomizes the values for the product and divide problems and sets the initial answers to 0, update the CheckTheAnswer method to evaluate the answer given for the product and divide problems, and update the timer1 event handler to evaluate the answers given for the product and divide problems

--- MathQuiz/Form1.cs
+++ MathQuiz/Form1.cs
@@ -27,6 +27,16 @@ namespace MathQuiz
         int minuend;
         int subtrahend;
 
+        // These integer variables store the numbers 
+        // for the multiplication problem. 
+        int multiplicand;
+        int multiplier;
+
+        // These integer variables store the numbers 
+        // for the division problem. 
+        int dividend;
+        int divisor;
+
         // This integer variable keeps track of the 
         // remaining time.
         int timeLeft;
@@ -65,6 +75,21 @@ namespace MathQuiz
             minusLeftLabel.Text = minuend.ToString();
             minusRightLabel.Text = subtrahend.ToString();
             difference.Value = 0;
+
+            // Fill in the multiplication problem.
+            multiplicand = randomizer.Next(2, 11);
+            multiplier = randomizer.Next(2, 11);
+            timesLeftLabel.Text = multiplicand.ToString();
+            timesRightLabel.Text = multiplier.ToString();
+            product.Value = 0;
+
+            // Fill in the division problem.
+            divisor = randomizer.Next(2, 11);
+            int temporaryQuotient = randomizer.Next(2, 11);
+            dividend = divisor * temporaryQuotient;
+            dividedLeftLabel.Text = dividend.ToString();
+            dividedRightLabel.Text = divisor.ToString();
+            quotient.Value = 0;
 
             // Start the timer.
             timeLeft = 30;
@@ -111,13 +136,15 @@ namespace MathQuiz
             }
             else
             {
-                // If the user ran out of time, stop the timer, show
+                // If the user ran out of time, stop the timer, show 
                 // a MessageBox, and fill in the answers.
                 timer1.Stop();
                 timeLabel.Text = "Time's up!";
-                MessageBox.Show("You didn't finish in time.", "Sorry!");
+                MessageBox.Show("You didn't finish in time.", "Sorry");
                 sum.Value = addend1 + addend2;
                 difference.Value = minuend - subtrahend;
+                product.Value = multiplicand * multiplier;
+                quotient.Value = dividend / divisor;
                 startButton.Enabled = true;
             }
         }
@@ -129,7 +156,9 @@ namespace MathQuiz
         private bool CheckTheAnswer()
         {
             if ((addend1 + addend2 == sum.Value)
-                && (minuend - subtrahend == difference.Value))
+                && (minuend - subtrahend == difference.Value)
+                && (multiplicand * multiplier == product.Value)
+                && (dividend / divisor == quotient.Value))
                 return true;
             else
                 return false;
