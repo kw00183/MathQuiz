a7515274c9b3e8eaf329a1a51418a95f4308e877
Kim Weible <kw00183@my.westga.edu>
2022-01-15 08:20:09 -0600

    add first row of controls for the addition problem to the form

--- MathQuiz/Form1.Designer.cs
+++ MathQuiz/Form1.Designer.cs
@@ -31,6 +31,12 @@ namespace MathQuiz
         {
             this.timeLabel = new System.Windows.Forms.Label();
             this.timeLeftLabel = new System.Windows.Forms.Label();
+            this.plusLeftLabel = new System.Windows.Forms.Label();
+            this.plusEqualSign = new System.Windows.Forms.Label();
+            this.plusRightLabel = new System.Windows.Forms.Label();
+            this.plusSIgn = new System.Windows.Forms.Label();
+            this.sum = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.sum)).BeginInit();
             this.SuspendLayout();
             // 
             // timeLabel
@@ -52,17 +58,72 @@ namespace MathQuiz
             this.timeLeftLabel.TabIndex = 1;
             this.timeLeftLabel.Text = "Time Left";
             // 
+            // plusLeftLabel
+            // 
+            this.plusLeftLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusLeftLabel.Location = new System.Drawing.Point(46, 68);
+            this.plusLeftLabel.Name = "plusLeftLabel";
+            this.plusLeftLabel.Size = new System.Drawing.Size(60, 50);
+            this.plusLeftLabel.TabIndex = 2;
+            this.plusLeftLabel.Text = "?";
+            this.plusLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // plusEqualSign
+            // 
+            this.plusEqualSign.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusEqualSign.Location = new System.Drawing.Point(244, 68);
+            this.plusEqualSign.Name = "plusEqualSign";
+            this.plusEqualSign.Size = new System.Drawing.Size(60, 50);
+            this.plusEqualSign.TabIndex = 4;
+            this.plusEqualSign.Text = "=";
+            this.plusEqualSign.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // plusRightLabel
+            // 
+            this.plusRightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusRightLabel.Location = new System.Drawing.Point(178, 68);
+            this.plusRightLabel.Name = "plusRightLabel";
+            this.plusRightLabel.Size = new System.Drawing.Size(60, 50);
+            this.plusRightLabel.TabIndex = 5;
+            this.plusRightLabel.Text = "?";
+            this.plusRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // plusSIgn
+            // 
+            this.plusSIgn.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusSIgn.Location = new System.Drawing.Point(112, 68);
+            this.plusSIgn.Name = "plusSIgn";
+            this.plusSIgn.Size = new System.Drawing.Size(60, 50);
+            this.plusSIgn.TabIndex = 6;
+            this.plusSIgn.Text = "+";
+            this.plusSIgn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.plusSIgn.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // sum
+            // 
+            this.sum.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sum.Location = new System.Drawing.Point(327, 76);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(100, 35);
+            this.sum.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.plusSIgn);
+            this.Controls.Add(this.plusRightLabel);
+            this.Controls.Add(this.plusEqualSign);
+            this.Controls.Add(this.plusLeftLabel);
             this.Controls.Add(this.timeLeftLabel);
             this.Controls.Add(this.timeLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Math Quiz";
+            ((System.ComponentModel.ISupportInitialize)(this.sum)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,6 +133,11 @@ namespace MathQuiz
 
         private System.Windows.Forms.Label timeLabel;
         private System.Windows.Forms.Label timeLeftLabel;
+        private System.Windows.Forms.Label plusLeftLabel;
+        private System.Windows.Forms.Label plusEqualSign;
+        private System.Windows.Forms.Label plusRightLabel;
+        private System.Windows.Forms.Label plusSIgn;
+        private System.Windows.Forms.NumericUpDown sum;
     }
 }
 
--- MathQuiz/Form1.cs
+++ MathQuiz/Form1.cs
@@ -16,5 +16,10 @@ namespace MathQuiz
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
