d5f51fece4bd23f108565fc0011a6a09450e17c0
Kim Weible <kw00183@my.westga.edu>
2022-01-15 08:14:42 -0600

    add in time left label with text

--- MathQuiz/Form1.Designer.cs
+++ MathQuiz/Form1.Designer.cs
@@ -29,22 +29,49 @@ namespace MathQuiz
         /// </summary>
         private void InitializeComponent()
         {
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.timeLeftLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.Location = new System.Drawing.Point(241, 9);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(200, 30);
+            this.timeLabel.TabIndex = 0;
+            // 
+            // timeLeftLabel
+            // 
+            this.timeLeftLabel.AutoSize = true;
+            this.timeLeftLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLeftLabel.Location = new System.Drawing.Point(134, 14);
+            this.timeLeftLabel.Name = "timeLeftLabel";
+            this.timeLeftLabel.Size = new System.Drawing.Size(101, 25);
+            this.timeLeftLabel.TabIndex = 1;
+            this.timeLeftLabel.Text = "Time Left";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.timeLeftLabel);
+            this.Controls.Add(this.timeLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Math Quiz";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label timeLeftLabel;
     }
 }
 
