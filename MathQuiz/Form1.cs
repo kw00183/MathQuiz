782d50d892eb8c163f4d504873f77ae602f61892
Kim Weible <kw00183@my.westga.edu>
2022-01-15 09:40:40 -0600

    add in a method to check the answer given called CheckTheAnswer and update the timer event handler to include a check in the if/else that evaluates the answer given with the time remaining

--- MathQuiz/Form1.cs
+++ MathQuiz/Form1.cs
@@ -78,16 +78,28 @@ namespace MathQuiz
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (timeLeft > 0)
+            if (CheckTheAnswer())
             {
-                // Display the new time left
-                // by updating the Time Left label.
-                timeLeft = timeLeft - 1;
+                // If CheckTheAnswer() returns true, then the user 
+                // got the answer right. Stop the timer  
+                // and show a MessageBox.
+                timer1.Stop();
+                MessageBox.Show("You got all the answers right!",
+                                "Congratulations!");
+                startButton.Enabled = true;
+            }
+            else if (timeLeft > 0)
+            {
+                // If CheckTheAnswer() return false, keep counting
+                // down. Decrease the time left by one second and 
+                // display the new time left by updating the 
+                // Time Left label.
+                timeLeft--;
                 timeLabel.Text = timeLeft + " seconds";
             }
             else
             {
-                // If the user ran out of time, stop the timer, show
+                // If the user ran out of time, stop the timer, show 
                 // a MessageBox, and fill in the answers.
                 timer1.Stop();
                 timeLabel.Text = "Time's up!";
@@ -95,6 +107,18 @@ namespace MathQuiz
                 sum.Value = addend1 + addend2;
                 startButton.Enabled = true;
             }
+        }
+
+        /// <summary>
+        /// Check the answer to see if the user got everything right.
+        /// </summary>
+        /// <returns>True if the answer's correct, false otherwise.</returns>
+        private bool CheckTheAnswer()
+        {
+            if (addend1 + addend2 == sum.Value)
+                return true;
+            else
+                return false;
         }
     }
 }
